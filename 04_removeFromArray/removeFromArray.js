388245a434dd77ca1dce617f961731e2fc10244f
Rakoski <mastrakoski@gmail.com>
2023-06-17 08:09:40 -0300

    My solutions till now!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
 
+    array.forEach(element => {
+        if(!args.includes(element)) {
+            newArray.push(element)
+        };
+    });
+    return newArray;
+
+    // return array.filter(val => !args.includes(val))
+    // outra solução
 };
 
 // Do not edit below this line
