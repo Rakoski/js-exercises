f209ef55138b3b280f4264bcc1a85456d6776cb5
Rakoski <mastrakoski@gmail.com>
2023-06-17 08:10:15 -0300

    My solutions till now

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // works with negative numbers
+    if (num < 0) {
+        return 'ERROR'
+    }
+    
+    let x = ""
+    for(i = 1; i <= num; i++) {
+        x += string
+    } 
+    return x
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    // string[::-1], in Python
+    let reverse = string.split("").reverse().join("");
+    return reverse
 };
 
 // Do not edit below this line
