ff94fc397da85966405708a8e47902d31857a8d5
Rakoski <mastrakoski@gmail.com>
2023-06-17 09:57:07 -0300

    Added new solutions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(firstElement, lastElement) {
 
+    const sum = function(array) {
+        let soma = 0
+        array.forEach(element => {
+            soma += element
+        });
+        return soma
+    }
+
+    if (!Number.isInteger(firstElement) || !Number.isInteger(lastElement)) return "ERROR";
+    if (firstElement < 0 || lastElement < 0) return "ERROR";
+    if(firstElement < lastElement) {
+        const toBeSummed = []
+
+        for(c = firstElement; c <= lastElement; c++) {
+            toBeSummed.push(firstElement++)
+        };
+        return sum(toBeSummed)
+
+    } else {
+        const toBeSummed = []
+
+        for(c = lastElement; c <= firstElement; c++) {
+            toBeSummed.push(lastElement++)
+        };
+        return sum(toBeSummed)
+        };
 };
 
 // Do not edit below this line
